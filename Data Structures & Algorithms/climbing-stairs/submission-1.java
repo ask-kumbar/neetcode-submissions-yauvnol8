68f0c152a05074b03551595f01e6b2cab47b5211
Akshay S K <48949692+ask-kumbar@users.noreply.github.com>
2026-05-22 22:50:25 +0530

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int climbStairs(int n) {
+        int[] ways = new int[n];
+        for (int i = 0; i < n; i++){
+            if ( i < 2){
+                ways[i] = i + 1;
+            }
+            else {
+                ways[i] = ways[i - 1] + ways[i - 2];
+            }
+        }
+        return ways[n-1];
+    }
+}
